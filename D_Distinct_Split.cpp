213d4b28cf8591ff8821615aa1e701a043753700
henriquecdb <henriquecamposj@gmail.com>
2023-02-10 10:12:04 -0300

    More CF solutions

---
+++ D_Distinct_Split.cpp
@@ -0,0 +1,25 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+typedef long long ll;
+typedef unsigned long long ull;
+
+void solve(int n, string s);
+
+int main()
+{
+    ios::sync_with_stdio(0);
+    cin.tie(0);
+    int t; cin >> t;
+    for (int i = 0; i < t; i++) {
+        int n; cin >> n;
+        string s; cin >> s;
+        solve(n, s);
+    }
+    return 0;
+}
+
+void solve(int n, string s) {
+    // code
+}
