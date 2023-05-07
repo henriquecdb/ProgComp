0882819ecf548beabe67dd24abde596878a1992d
henriquecdb <henriquecamposj@gmail.com>
2023-05-07 10:46:40 -0300

    Add LeetCode solutions

---
+++ CodeForces/217_ContainsDuplicate.cpp
@@ -0,0 +1,14 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+class Solution {
+public:
+    bool containsDuplicate(vector<int>& nums) {
+      set<int> s;
+      for (int i = 0; i < nums.size(); i++) {
+        s.insert(nums[i]);   
+      }
+      return (nums.size() == s.size());
+    }
+};
